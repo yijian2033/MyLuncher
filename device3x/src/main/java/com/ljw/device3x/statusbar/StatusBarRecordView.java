d7081bee9659acce03f20d294888edf65ace5ca1
fawei.gui <fawei.gui@conqueror.cn>
2017-08-29 10:27:42 +0800

    改成观察者模式监听录像状态

--- device3x/src/main/java/com/ljw/device3x/statusbar/StatusBarRecordView.java
+++ device3x/src/main/java/com/ljw/device3x/statusbar/StatusBarRecordView.java
@@ -1,9 +1,13 @@
 package com.ljw.device3x.statusbar;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.ContentObserver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.Message;
 import android.provider.Settings;
@@ -23,18 +27,19 @@ import java.util.TimerTask;
  * Created by Administrator on 2016/12/22 0022.
  */
 
-public class StatusBarRecordView extends ImageView{
+public class StatusBarRecordView extends ImageView {
     public static final String RAYEE_RECORD_STATE = "DvrRecordState";
     private static final String DEV_REC_ON = "cn.conqueror.action.DVR_REC_ON";//行车记录仪打开
     private static final String DEV_REC_OFF = "cn.conqueror.action.DVR_REC_OFF";//行车记录仪关闭
     private static final int REC_OFF_IMG = R.mipmap.videocamera_off;
     private static final int[] REC_ON_IMG = {R.mipmap.videocamera_on, R.mipmap.videocamera_off};
-
+    private static final Uri STATE_URI = Uri.parse("content://com.conqueror.dvr.contentprovider/recordState");
     private Context context;
+    private  final String TAG = "StatusBarRecordView";
 
     private static final int REC_ON = 1;
     private static final int REC_OFF = 0;
-    private  int count = 0;//图片循环计数器
+    private int count = 0;//图片循环计数器
     private boolean isStop = false;
 
     private RecHandler recHandler;
@@ -50,10 +55,11 @@ public class StatusBarRecordView extends ImageView{
         this(context, null);
     }
 
-    private  class RecHandler extends Handler {
+    private class RecHandler extends Handler {
         WeakReference<StatusBarRecordView> mView;
         private RecStartTimerTask recStartTimerTask;
         public Timer recStartTimer;
+
         public RecHandler(StatusBarRecordView view) {
             mView = new WeakReference<StatusBarRecordView>(view);
             recStartTimer = new Timer(true);
@@ -63,16 +69,17 @@ public class StatusBarRecordView extends ImageView{
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
 
-            if(mView.get() == null)
+            if (mView.get() == null)
                 return;
             StatusBarRecordView view = mView.get();
-            if(msg.what == REC_OFF) {
-                if(recStartTimer != null && recStartTimerTask != null)
+            if (msg.what == REC_OFF) {
+                if (recStartTimer != null && recStartTimerTask != null)
                     recStartTimerTask.cancel();
                 view.setImageResource(REC_OFF_IMG);
             } else {
-                if(count >= REC_ON_IMG.length)
+                if (count >= REC_ON_IMG.length)
                     count = 0;
+                Log.i(TAG, "view.setImageResource"+count);
                 view.setImageResource(REC_ON_IMG[count++]);
             }
         }
@@ -80,7 +87,7 @@ public class StatusBarRecordView extends ImageView{
     }
 
     private void startRecyleRecImg() { //当行车记录仪打开时打开图片切换
-        if(recHandler.recStartTimer != null && recHandler.recStartTimerTask != null)
+        if (recHandler.recStartTimer != null && recHandler.recStartTimerTask != null)
             recHandler.recStartTimerTask.cancel();
         recHandler.recStartTimerTask = new RecStartTimerTask();
         recHandler.recStartTimer.schedule(recHandler.recStartTimerTask, 1000, 1000);
@@ -94,48 +101,58 @@ public class StatusBarRecordView extends ImageView{
     }
 
 
-    private BroadcastReceiver recReveive = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if(action.equals(DEV_REC_OFF)) {
-                recHandler.sendEmptyMessage(REC_OFF);
-             //   Toast.makeText(context,"存储录像状态-关闭",Toast.LENGTH_SHORT).show();
-            //    Settings.System.putInt(getContext().getContentResolver(),RAYEE_RECORD_STATE,REC_OFF);
-            } else{
-                startRecyleRecImg();
-             //   Toast.makeText(context,"存储录像状态-打开",Toast.LENGTH_SHORT).show();
-             //   Settings.System.putInt(getContext().getContentResolver(),RAYEE_RECORD_STATE,REC_ON);
-            }
-
-
-        }
-    };
-
 
 
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-        IntentFilter intentFilter = new IntentFilter(DEV_REC_ON);
-        intentFilter.addAction(DEV_REC_OFF);
-        context.registerReceiver(recReveive, intentFilter);
+         ContentResolver resolver = context.getContentResolver();
+        resolver.registerContentObserver(STATE_URI, true, observer);
+
         initState();
     }
 
     private void initState() {
-        int state = Settings.System.getInt(getContext().getContentResolver(),RAYEE_RECORD_STATE,-1);
-     //   Toast.makeText(context,"获取录像状态："+state,Toast.LENGTH_SHORT).show();
-        if (state == 0) {
-            recHandler.sendEmptyMessage(state);
-        }else if (state == 1) {
-            startRecyleRecImg();
+        ContentResolver resolver = context.getContentResolver();
+        Cursor cursor = resolver.query(STATE_URI, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            int state = cursor.getInt(cursor.getColumnIndex("state"));
+            Log.i(TAG, "recordState change:"+state);
+            if (state == 0){
+                recHandler.sendEmptyMessage(REC_OFF);
+            }else if (state == 1){
+                startRecyleRecImg();
+            }
         }
     }
 
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
-        context.unregisterReceiver(recReveive);
+       ContentResolver resolver = context.getContentResolver();
+        resolver.unregisterContentObserver(observer);
     }
+
+
+    private ContentObserver observer = new ContentObserver(new Handler()) {
+
+
+        // 得到数据的变化通知，该方法只能粗略知道数据的改变，并不能判断是哪个业务操作进行的改变
+        @Override
+        public void onChange(boolean selfChange) {
+            // select * from person order by id desc limit 1 // 取得最近插入的值（序号大——>小并取第一个）
+            ContentResolver resolver = getContext().getContentResolver();
+            Cursor cursor = resolver.query(STATE_URI, null, null, null, null);
+            if (cursor.moveToFirst()) {
+                int state = cursor.getInt(cursor.getColumnIndex("state"));
+                Log.i(TAG, "recordState change:"+state);
+                if (state == 0){
+                    recHandler.sendEmptyMessage(REC_OFF);
+                }else if (state == 1){
+                    startRecyleRecImg();
+                }
+            }
+        }
+
+    };
 }
